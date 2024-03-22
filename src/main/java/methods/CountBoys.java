bd6092ccda5d70163d1c1b4a616505031d45f186
zenoooooooooo <esplanaejhaym@gmail.com>
2024-03-22 19:13:32 +0800

    Added functionality to calculate the total presence per day

--- src/main/java/methods/CountBoys.java
+++ src/main/java/methods/CountBoys.java
@@ -5,6 +5,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -85,7 +86,41 @@ public class CountBoys {
                             cell = mergedRegion.getLastColumn();
                         }
                 		
-                                  
+                        if (cellIteration > 2 + cd.getBlanks() && cellIteration <= 2 + cd.getBlanks() + cd.getNumberOfDates()) {
+                            int rowPerDayIteration = 0;
+                            int presencePerDay = 0;
+                            for (int row2 = currentCell.getAddress().getRow(); row2 <= start.getRow() + cs.getBoysNumber(); row2++) {
+                                rowPerDayIteration++;
+                                Row currentRow2 = sheet.getRow(row2);
+                                Cell currentCell2 = currentRow2.getCell(cell);
+                                
+                                // Check if the current cell is within a merged region
+                                CellRangeAddress mergedRegion2 = null;
+                                for (int i = 0; i < sheet.getNumMergedRegions(); i++) {
+                                    CellRangeAddress region = sheet.getMergedRegion(i);
+                                    if (region.isInRange(row2, cell)) {
+                                        mergedRegion2 = region;
+                                        break;
+                                    }
+                                }
+                                
+                                if (mergedRegion2 != null) {
+                                    
+                                    int firstColumn = mergedRegion2.getFirstColumn();
+                                    int firstRow = mergedRegion2.getFirstRow();
+                                    currentCell2 = sheet.getRow(firstRow).getCell(firstColumn);
+                                }
+                                
+                                if (currentCell2.getCellType().equals(CellType.BLANK) && rowPerDayIteration < cs.getBoysNumber() + 1) {
+                                    presencePerDay++;
+                                }
+                                if (rowPerDayIteration == cs.getBoysNumber() + 1) {
+                                    currentCell2.setCellValue(presencePerDay);
+                                    System.out.println(currentCell2.getNumericCellValue() + ": " + currentCell2.getAddress());
+                                }
+                            }
+                        }
+
                         
                 		if (rowIteration <= cs.getBoysNumber() && cellIteration > 2 && cellIteration < 28 && currentCell.getStringCellValue().trim().equalsIgnoreCase("x")) {
                 			studentAbsences++;
