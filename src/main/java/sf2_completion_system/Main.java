f574fd49e627f0d9156384c37b9c200d92f4663d
lnxpsyhi <rommelmatic21@gmail.com>
2024-03-26 01:54:32 +0800

    design

--- src/main/java/sf2_completion_system/Main.java
+++ src/main/java/sf2_completion_system/Main.java
@@ -8,7 +8,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 
 public class Main extends Application {
@@ -38,12 +40,12 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-		scene = new Scene(loadFXML("excchooser"));
+		scene = new Scene(loadFXML("sf2"));
         primaryStage.setScene(scene);
-        primaryStage.setMinWidth(200);
-        primaryStage.setMinHeight(200);
         primaryStage.setTitle("Student Form 2 Completion System");
         primaryStage.getIcons().add(new Image(new FileInputStream(IMGICON)));
+        primaryStage.setResizable(false);
+        primaryStage.initStyle(StageStyle.UTILITY);
         primaryStage.show();
 	}
 	
--- src/main/java/sf2_completion_system/SF2GUI.java
+++ src/main/java/sf2_completion_system/SF2GUI.java
@@ -1,13 +1,34 @@
 package sf2_completion_system;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
 
 public class SF2GUI implements Initializable {
+	
+	@FXML
+	private void open() {
+		System.out.println(chooseFile());
+	}
+	
+	private String chooseFile() {
+		FileChooser chooser = new FileChooser();
+		File file = chooser.showOpenDialog(Window.getWindows().get(0));
+		String filePath = file != null ? file.getAbsolutePath() : "";
+		return filePath;
+	}
+	
+	
+	@FXML
+	private void exit() {
+		System.exit(0);
+	}
 	
 	@FXML
 	private void toSecondary() throws IOException {
