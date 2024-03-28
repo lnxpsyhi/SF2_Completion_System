df802c4f4a171fbfddcbcd908c34ad3ab4a23124
lnxpsyhi <rommelmatic21@gmail.com>
2024-03-28 19:26:15 +0800

    some improvements

--- src/main/java/sf2_completion_system/Main.java
+++ src/main/java/sf2_completion_system/Main.java
@@ -1,4 +1,5 @@
 package sf2_completion_system;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -15,31 +16,30 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-
 public class Main extends Application {
-	
+
 	private double xOffset;
 	private double yOffset;
-	
+
 	private static final File IMGICON = new File("src/main/resources/images/file.png");
-	
+
 	private static Scene scene;
-	
+
 	static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-	
+		scene.setRoot(loadFXML(fxml));
+	}
+
 	private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-	
+		FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxml + ".fxml"));
+		return fxmlLoader.load();
+	}
+
 	public static void main(String[] args) {
-		
+
 		launch(args);
 
-		//CountOverall co = new CountOverall();
-		//co.countOverallTotalAbsences(PATH, "A18:BO78", "K16:BH16", 1);
+		// CountOverall co = new CountOverall();
+		// co.countOverallTotalAbsences(PATH, "A18:BO78", "K16:BH16", 1);
 
 	}
 
@@ -48,12 +48,12 @@ public class Main extends Application {
 		// TODO Auto-generated method stub
 		scene = new Scene(loadFXML("sf2"));
 		scene.setOnMousePressed(new EventHandler<MouseEvent>() {
-		    @Override
-		    public void handle(MouseEvent event) {
-		    	scene.setCursor(Cursor.OPEN_HAND);
-		        xOffset = event.getSceneX();
-		        yOffset = event.getSceneY();
-		    }
+			@Override
+			public void handle(MouseEvent event) {
+				scene.setCursor(Cursor.OPEN_HAND);
+				xOffset = event.getSceneX();
+				yOffset = event.getSceneY();
+			}
 		});
 		scene.setOnMouseDragged(new EventHandler<MouseEvent>() {
 
@@ -71,13 +71,13 @@ public class Main extends Application {
 				// TODO Auto-generated method stub
 				scene.setCursor(Cursor.DEFAULT);
 			}
-			
+
 		});
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Student Form 2 Completion System");
-        primaryStage.getIcons().add(new Image(new FileInputStream(IMGICON)));
-        primaryStage.initStyle(StageStyle.UNDECORATED);
-        primaryStage.show();
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Student Form 2 Completion System");
+		primaryStage.getIcons().add(new Image(new FileInputStream(IMGICON)));
+		primaryStage.initStyle(StageStyle.UNDECORATED);
+		primaryStage.show();
 	}
-	
+
 }
